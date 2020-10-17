7138d6cf1d8c5db2e204f8c017c2f8c6b3927f46
Paul Ticona Acero <paulticona264riddick@gmail.com>
2020-10-16 23:39:43 -0500

    creando banner y cards mediante componetes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,36 @@
 import React from 'react';
 import "./styles/styles.scss"
+import Componente2 from './Componente2'
 
 const App = ()  => (
-  <>
-    <div classNameName="main-banner img-container" id="main-banner">
-        <div classNameName="ed-grid lg-grid-6">
-          <div classNameName="lg-cols-4 lg-x-2">
-            <img classNameName="main-banner__img" alt="banner" src="https://lh3.googleusercontent.com/proxy/xaVnjMWpPVRHFtZmrsDn3CdojK6javSXiEZaGt1oZFn1gyOvBpwGnLxyDTaxqY2Vca-fKlreggSlVTRjJPnA1VmMUeU0M3MgeSSvY-NinUzxRTDID0qww4uR5RW2l7g"/>
-            <div classNameName="main-banner__data s-center">
-              <p classNameName="t2 s-mb-0">Título del banner</p>
-              <p> Subtítulo del banner</p>
-              <a href="https://ed.team"  classNameName="button">Botón del banner</a>
-            </div>
-          </div>
-        </div>
+<>
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="banner" src="https://s.yimg.com/ny/api/res/1.2/bzzlTjOjXE1oiDztUzyl9g--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NjAwO2g9MzAw/https://media.zenfs.com/es/levelup_525/7938bc5369329b58cc6c6aa4f373fc3a"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Darksiders</p>
+        <p> I II III y IV</p>
+        <a href="https://es.wikipedia.org/wiki/Darksiders" className="button dark-color">Jugar</a>
       </div>
-      <div>
-        <article className="card">
-          <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg" alt=""/>
-          </div>
-          <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="t5 s-mb-2 s-center">
-              Programación orientada a objetos con Go
-            </h3>
-            <div className="s-mb-2 s-main-center">
-              <div className="card__teacher s-cross-center">
-                <div className="card__avatar s-mr-1">
-                  <div className="circle img-container">
-                    <img src="" alt=""/>
-                  </div>
-                </div>
-                <span className="small">Alexys Lozada</span>
-              </div>
-            </div>
-            <div className="s-main-center">
-              <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-            </div>
-          </div>
-        </article>
-      </div>
- </>
+    </div>
+  </div>
+</div>
+<div className="ed-grid m-grid-3">
+  <Componente2 />
+  <Componente2 />
+  <Componente2 />
+  <Componente2 />
+  <Componente2 />
+</div>
+</>
 )
 
 /*Reglas JSX
   1.- Toda etiqueta debe cerrarce ( />)
   2.- Los componentes deben devolver un solo elemnto Padre
   3.- apoyarce de los elementos cuando necesito devolver dos elementos (<></> o fagment)
-  4.- className = "" => classNameName = "" Tambien for => htmlFor
+  4.- classNameName = "" => classNameNameName = "" Tambien for => htmlFor
 */
 
 export default App;
